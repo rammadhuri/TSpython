351bd196fd0e27d4c5d07e4fa07b7600aa734c26
rammadhuri <56595826+rammadhuri@users.noreply.github.com>
2024-05-23 00:18:29 +0530

    Update triangle_.py

--- triangle_.py
+++ triangle_.py
@@ -5,7 +5,7 @@ of its sides (equilateral, isosceles, or scalene)."""
 def triangle_type(s1, s2, s3):
     if s1 == s2 == s3:
         print("equilateral traingle")
-    elif s1 == s2 != s3:
+    elif s1 == s2 or s1== s3 or s2==s3:
         print("isosceles traingle")
     else:
         print("scalene traingle")
